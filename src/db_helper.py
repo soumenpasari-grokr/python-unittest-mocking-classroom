d98e68022d786719bd0b8a6416f8553c3238307d
soumenpasari-grokr <soumen.pasari@datagrokr.com>
2019-12-27 15:10:22 +0530

    feat : added max and min salary function

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,15 +1,27 @@
+import mysql.connector
+
 class DbHelper:
+    def __init__(self):
+        self.conn = mysql.connector.connect(host='localhost',user='root',passwd='root',database='employees')
+        self.cursor = self.conn.cursor()
+
     def get_maximum_salary(self):
         '''
         Implement the logic to find and return maximum salary from employee table
         '''
-        return None
+        query = 'select max(salary) as max_salary from salaries'
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result[0]
 
     def get_minimum_salary(self):
         '''
         Implement the logic to find and return minimum salary from employee table
         '''
-        return None
+        query = 'select min(salary) as min_salary from salaries'
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result[0]
 
 if __name__ == "__main__":
     db_helper = DbHelper()
